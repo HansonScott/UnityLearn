ae67058c30dd3887e669b9011e744d85b0d9fa22
Scott Hanson <shanson77@aol.com>
2021-03-11 11:08:23 -0800

    testing a change to gamemaster script.

--- GitTest/Assets/Scripts/GameManager.cs
+++ GitTest/Assets/Scripts/GameManager.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    // testing a change for Git practice
+
+
     // Start is called before the first frame update
     void Start()
     {
